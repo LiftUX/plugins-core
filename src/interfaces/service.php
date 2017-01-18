79dd4f0e1fed351d91e93f29761e1f9baf7ce372
Christian Chung <christian.m.chung@gmail.com>
2017-01-18 15:09:25 -0500

    interface(service): Add service interface

---
+++ src/interfaces/service.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Service
+ *
+ * @since  v0.1.0
+ *
+ * @package  Lift\Core
+ * @subpackage  Interfaces
+ */
+
+namespace Lift\Core\Interfaces;
+
+/**
+ * Interface: Service
+ *
+ * @since  v0.1.0
+ */
+interface Service{
+
+	/**
+	 * Call
+	 *
+	 * @since  v.0.1.0
+	 * @param  string $type    The type of call to make.
+	 * @param  array  ...$args Arguments to configure the call.
+	 * @return mixed           Response from call.
+	 */
+	public function call( $type, ...$args );
+
+}
