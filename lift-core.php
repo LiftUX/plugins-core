aed807ee1282b4208b9b05a679c7430c026d61e8
Christian Chung <christian.m.chung@gmail.com>
2017-01-26 21:06:19 -0500

    chore(build): Add build files

---
+++ lift-core.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Lift Core
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     lift-core
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Lift_Core
+ */
