a8d94d363cb1295671a9c78924a78e4d3ba6583f
Christian Chung <christian.m.chung@gmail.com>
2017-02-23 15:04:24 -0500

    functions(*): Add a constant compare helper function

---
+++ src/classes/exceptions/class-file-loader-exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File Loader Exception
+ *
+ * @package  Lift|Core
+ * @subpackage  Exceptions
+ */
+
+namespace Lift\Core\Exceptions;
+
+/**
+ * Class: File Loader Exception
+ *
+ * @since  v0.1.0
+ */
+class File_Loader_Exception extends \Exception {}
---
+++ src/functions/functions-runtime.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Runtime Functions
+ *
+ * A collection of functions that help execution at runtime.
+ */
+
+namespace Lift\Core\Functions\Runtime_Utils;
+
+/**
+ * Constant Compare
+ *
+ * @since  v0.1.0
+ * @param  string $constant Constant Name
+ * @param  mixed  $compare  The value of the constant to compare against.
+ * @return bool             True if the value of the constant is equal to the comparator, false otherwise.
+ */
+function const_compare( string $constant, $compare ) : bool {
+	if ( defined ( $constant ) && constant( $constant ) === $compare ) {
+		return true;
+	}
+	return false;
+}
