6e6046778785cce7329f4020f129668448a09bb0
Christian Chung <christian.m.chung@gmail.com>
2017-05-17 14:25:26 -0400

    file(main): Tweak autoloading

--- lift-core.php
+++ lift-core.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name:     Lift Core
  *
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://liftux.com
+ * Description:     A PHP library for composing WordPress themes and applications.
+ * Author:          Christian Chung
+ * Author URI:      https://liftux.com
  * Text Domain:     lift-core
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -14,3 +14,20 @@
  */
 
 namespace Lift\Core;
+
+// This is a library, all we do is autoload the library classes.
+if ( ! class_exists( 'Lift\Core\Hook_Catalog' ) ) {
+	require_once( 'vendor/autoload.php' );
+}
+
+/**
+ * Ready
+ *
+ * Alerts WordPress that the library is loaded and ready.
+ *
+ * @return void
+ */
+function ready() {
+	do_action( 'lift_init' );
+}
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\ready' );
