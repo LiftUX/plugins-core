348771eceab32a4002c67c5073390de8bb65afe8
Christian Chung <christian.m.chung@gmail.com>
2017-06-23 10:28:05 -0400

    class(assets): Fix phpcs sniffs

--- src/classes/class-assets.php
+++ src/classes/class-assets.php
@@ -12,11 +12,24 @@ namespace Lift\Core;
 /**
  * Class: Assets
  *
+ * A class that you can add instances of Asset_Loader to.  Handles searching through multiple Asset_Loaders
+ * to resolve an asset dependency's path, uri, or contents.
+ *
  * @since v0.1.0
  */
 final class Assets extends \SplPriorityQueue {
+	/**
+	 * Instance
+	 *
+	 * @var Assets
+	 */
 	public static $instance = null;
 
+	/**
+	 * Factory
+	 *
+	 * @return Assets
+	 */
 	public static function factory() : Assets {
 		if ( is_null( self::$instance ) ) {
 			self::$instance = new self;
@@ -24,11 +37,24 @@ final class Assets extends \SplPriorityQueue {
 		return self::$instance;
 	}
 
+	/**
+	 * Add Loader
+	 *
+	 * @param Asset_Loader $loader   An instance of Asset_Loader.
+	 * @param int|integer  $priority The priority of the Asset_Loader, higher is greater priority.
+	 * @return void
+	 */
 	public static function add_loader( Asset_Loader $loader, int $priority = 10 ) {
 		self::factory()->insert( $loader, $priority );
 	}
 
-	public static function get_path( $filename ) {
+	/**
+	 * Get Path
+	 *
+	 * @param  string $filename The filename of the asset you're looking for.
+	 * @return string
+	 */
+	public static function get_path( string $filename ) : string {
 		while ( self::factory()->valid() ) {
 			try {
 				$path = self::factory()->current()->get_path( $filename );
@@ -45,7 +71,13 @@ final class Assets extends \SplPriorityQueue {
 		return '';
 	}
 
-	public static function get_uri( $filename ) {
+	/**
+	 * Get URI
+	 *
+	 * @param  string $filename The filename of the asset you're looking for.
+	 * @return string
+	 */
+	public static function get_uri( string $filename ) : string {
 		while ( self::factory()->valid() ) {
 			try {
 				$path = self::factory()->current()->get_uri( $filename );
@@ -62,7 +94,13 @@ final class Assets extends \SplPriorityQueue {
 		return '';
 	}
 
-	public static function get_contents( $filename ) {
+	/**
+	 * Get Content
+	 *
+	 * @param  string $filename The filename of the asset you're looking for.
+	 * @return string
+	 */
+	public static function get_contents( string $filename ) : string {
 		while ( self::factory()->valid() ) {
 			try {
 				$path = self::factory()->current()->get_contents( $filename );
